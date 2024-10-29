25aca976ec61cb0fefeafb70511df46777a62b68
Jesper van den Ende <jespertheend@gmail.com>
2024-10-29 16:00:28 +0100

    fix: Use quoted props to support property mangling

--- src/mod.js
+++ src/mod.js
@@ -1,6 +1,35 @@
 export function crazyGamesPlugin() {
 	let initializeCalled = false;
 
+	const props = /** @type {const} */ ({
+		CrazyGames: "CrazyGames",
+		SDK: "SDK",
+		game: "game",
+		ad: "ad",
+		banner: "banner",
+		gameplayStart: "gameplayStart",
+		gameplayStop: "gameplayStop",
+		sdkGameLoadingStart: "sdkGameLoadingStart",
+		sdkGameLoadingStop: "sdkGameLoadingStop",
+		happytime: "happytime",
+		inviteLink: "inviteLink",
+		requestAd: "requestAd",
+		requestResponsiveBanner: "requestResponsiveBanner",
+		adFinished: "adFinished",
+		adError: "adError",
+		adStarted: "adStarted",
+	});
+
+	// @ts-ignore We want to make sure that `props` remains an object.
+	// Normally, terser would turn every property into a separate variable.
+	// This would be fine for the first pass of minification, but if a user
+	// were to bundle and minify this libarry with their own code, they will minify a second time
+	// causing all these props to lose their quotes.
+	// This can be an issue if the new bundle has property mangling enabled.
+	// This if statement will never run, but rollup and terser will both think it
+	// might and so the `props` opbject will remain an object.
+	if (props > 0) console.log(props);
+
 	/** @type {CrazySdkInstance} */
 	let sdk;
 	/** @type {import("$adlad").AdLadPluginInitializeContext} */
@@ -12,7 +41,7 @@ export function crazyGamesPlugin() {
 	async function showVideoAd(type) {
 		/** @type {Promise<import("$adlad").ShowFullScreenAdResult>} */
 		const promise = new Promise((resolve) => {
-			sdk.ad.requestAd(type, {
+			sdk[props.ad][props.requestAd](type, {
 				adError(e) {
 					if (e == "Ad requested too soon") {
 						resolve({
@@ -68,21 +97,21 @@ export function crazyGamesPlugin() {
 				sdkUrl.searchParams.set("useLocalSdk", "true");
 			}
 			await import(sdkUrl.href);
-			sdk = window.CrazyGames.SDK;
+			sdk = window[props.CrazyGames][props.SDK];
 		},
 		manualNeedsPause: true,
 		manualNeedsMute: true,
 		async loadStart() {
-			await sdk.game.sdkGameLoadingStart();
+			await sdk[props.game][props.sdkGameLoadingStart]();
 		},
 		async loadStop() {
-			await sdk.game.sdkGameLoadingStop();
+			await sdk[props.game][props.sdkGameLoadingStop]();
 		},
 		async gameplayStart() {
-			await sdk.game.gameplayStart();
+			await sdk[props.game][props.gameplayStart]();
 		},
 		async gameplayStop() {
-			await sdk.game.gameplayStop();
+			await sdk[props.game][props.gameplayStop]();
 		},
 		async showFullScreenAd() {
 			return await showVideoAd("midgame");
@@ -91,11 +120,11 @@ export function crazyGamesPlugin() {
 			return await showVideoAd("rewarded");
 		},
 		showBannerAd(options) {
-			sdk.banner.requestResponsiveBanner(options.id);
+			sdk[props.banner][props.requestResponsiveBanner](options.id);
 		},
 		customRequests: {
 			happyTime() {
-				sdk.game.happytime();
+				sdk[props.game][props.happytime]();
 			},
 			/**
 			 * @param {ConstructorParameters<typeof URLSearchParams>} args
@@ -107,7 +136,7 @@ export function crazyGamesPlugin() {
 				for (const [key, value] of urlParams.entries()) {
 					paramsObj[key] = value;
 				}
-				return await sdk.game.inviteLink(paramsObj);
+				return await sdk[props.game][props.inviteLink](paramsObj);
 			},
 		},
 	});
